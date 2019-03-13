ac5dbeb308a26bc6c696d955607bac226e331ab4
saakur <saakur@mail.usf.edu>
2019-03-13 17:08:11 -0400

    update

--- yolo_video.py
+++ yolo_video.py
@@ -12,6 +12,7 @@ def detect_img(yolo):
             print('Open Error! Try again!')
             continue
         else:
+            image.resize((416,416), Image.ANTIALIAS)
             r_image = yolo.detect_image(image)
             # r_image.show()
             r_image.save('detections.png')
