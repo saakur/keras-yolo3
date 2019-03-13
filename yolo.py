0dda50078f8cca2c72c0b3de6471ef46d1bea7b0
saakur <saakur@mail.usf.edu>
2019-03-13 17:04:30 -0400

    update

--- yolo.py
+++ yolo.py
@@ -119,7 +119,7 @@ class YOLO(object):
                               image.height - (image.height % 32))
             boxed_image = letterbox_image(image, new_image_size)
         image_data = np.array(boxed_image, dtype='float32')
-        image_data = np.array(image, dtype='float32')
+        # image_data = np.array(image, dtype='float32')
         print(image_data.shape)
         image_data /= 255.
         image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
