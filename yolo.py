23b0658ea2b31826680fb8a3f3864d1a7f88107a
saakur <saakur@mail.usf.edu>
2019-03-17 19:02:22 -0400

    update

--- yolo.py
+++ yolo.py
@@ -106,7 +106,7 @@ class YOLO(object):
         #     if self.yolo_model.layers[i].name in keys:
         #         self.yolo_model.layers[i].set_weights()
 
-        sys.exit(0)
+        # sys.exit(0)
         # self.yolo_model.compile()
         # self.yolo_model.load_weights('/data/saakur/keras-yolo3/logs/000/ep001-loss59.197-val_loss32.265_1.h5')
         # self.yolo_model.load_weights(self.model_path) # make sure model, anchors and classes match
