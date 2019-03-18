27809b0b96b8c5afd02df282fdd589e09acc8d54
saakur <saakur@mail.usf.edu>
2019-03-17 20:31:36 -0400

    update

--- yolo.py
+++ yolo.py
@@ -71,11 +71,13 @@ class YOLO(object):
         except:
             self.yolo_model = tiny_yolo_body(Input(shape=(None,None,3)), num_anchors//2, num_classes) \
                 if is_tiny_version else yolo_body(Input(shape=(None,None,3)), num_anchors//3, num_classes)
-            self.yolo_model.load_weights(self.model_path) # make sure model, anchors and classes match
+            # self.yolo_model.load_weights(self.model_path) # make sure model, anchors and classes match
         else:
             assert self.yolo_model.layers[-1].output_shape[-1] == \
                 num_anchors/len(self.yolo_model.output) * (num_classes + 5), \
                 'Mismatch between model and given anchor and class sizes'
+        
+        load_multigpu_checkpoint_weights(self.yolo_model, self.model_path)
 
         print('{} model, anchors, and classes loaded.'.format(model_path))
 
@@ -168,6 +170,50 @@ class YOLO(object):
 
     def close_session(self):
         self.sess.close()
+
+def load_multigpu_checkpoint_weights(model, h5py_file):
+    """
+    Loads the weights of a weight checkpoint from a multi-gpu
+    keras model.
+
+    Input:
+
+        model - keras model to load weights into
+
+        h5py_file - path to the h5py weights file
+
+    Output:
+        None
+    """
+
+    print("Setting weights...")
+    with h5py.File(h5py_file, "r") as file:
+
+        # Get model subset in file - other layers are empty
+        weight_file = file["model_1"]
+
+        for layer in model.layers:
+
+            try:
+                layer_weights = weight_file[layer.name]
+
+            except:
+                # No weights saved for layer
+                continue
+
+            try:
+                weights = []
+                # Extract weights
+                for term in layer_weights:
+                    if isinstance(layer_weights[term], h5py.Dataset):
+                        # Convert weights to numpy array and prepend to list
+                        weights.insert(0, np.array(layer_weights[term]))
+
+                # Load weights to model
+                layer.set_weights(weights)
+
+            except Exception as e:
+                print("Error: Could not load weights for layer:", layer.name, file=stderr)
 
 def detect_video(yolo, video_path, output_path=""):
     import cv2
