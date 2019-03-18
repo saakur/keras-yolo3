42cdf49e874f3ecc4d5d45929cf46f51658327d8
saakur <saakur@mail.usf.edu>
2019-03-17 20:01:15 -0400

    update

--- yolo.py
+++ yolo.py
@@ -77,7 +77,7 @@ class YOLO(object):
         #         num_anchors/len(self.yolo_model.output) * (num_classes + 5), \
         #         'Mismatch between model and given anchor and class sizes'
         self.yolo_model = yolo_body(Input(shape=(None,None,4)), num_anchors//3, num_classes)
-        self.yolo_model.load_weights(self.model_path, by_name=True)
+        self.yolo_model.load_weights(self.model_path)
 
         print('{} model, anchors, and classes loaded.'.format(model_path))
 
