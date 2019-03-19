35513081474b4398259f9c56716e233b784c38ff
saakur <saakur@mail.usf.edu>
2019-03-19 14:18:48 -0400

    update

--- yolo.py
+++ yolo.py
@@ -40,6 +40,7 @@ class YOLO(object):
     def __init__(self, **kwargs):
         self.__dict__.update(self._defaults) # set up default values
         self.__dict__.update(kwargs) # and update with user overrides
+        print(self.__dict__)
         self.class_names = self._get_class()
         self.anchors = self._get_anchors()
         self.sess = K.get_session()
